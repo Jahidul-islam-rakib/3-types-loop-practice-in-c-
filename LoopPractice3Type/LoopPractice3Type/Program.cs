0d02b256abc4c1f6930cca2b000a3cff78e36003
Jahidul-islam-rakib <168633979+Jahidul-islam-rakib@users.noreply.github.com>
2024-06-03 22:36:01 +0500

    Add files via upload

---
+++ LoopPractice3Type/LoopPractice3Type/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoopPractice3Type
+{
+    public class Loop3Type
+    {
+        static void Main(string[] args)
+        {
+            /* Write a C# program to print the numbers from 1 to 10 using three different loop
+             * constructs: for, while, and do-while*/
+
+                /* print 1 to 10 using for loop*/
+                Console.WriteLine("print 1 to 10 using for loop : ---->");
+                for (int a = 1; a <= 10; a++)
+                {
+                    Console.Write(a + " ");
+                }
+
+
+                /* print 1 to 10 using while loop*/
+                Console.WriteLine("\n\nprint 1 to 10 using while loop : ---->");
+                int num = 1;
+                while (num <= 10)
+                {
+                    Console.Write(num + " ");
+                    num++;
+                }
+
+
+                /* print 1 to 10 using do while loop*/
+                Console.WriteLine("\n\nprint 1 to 10 using do while loop : ----> ");
+                num = 1;
+                do
+                {
+                    Console.Write(num + " ");
+                    num++;
+                } while (num <= 10);
+            Console.WriteLine("\n\n");
+
+
+
+        }
+    }
+}
+    
+
